e43c74b5e6ced256af9b4e10cdd0e516316e577c
pythonappend <pythonappend@gmail.com>
2013-08-12 03:32:04 -0700

    Create c.cpp

---
+++ c.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+  int a,b,c;
+  
+  cout << "first number: ";
+  cin >> a;
+  cout << "second number: ";
+  cin >> b;
+  
+  c = a + b;
+  
+  cout << "Result is " << c << endl;
+  
+  return 0;
+
+}
